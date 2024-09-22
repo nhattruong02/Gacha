49a146bb9259bdb2ec2092bd9d201de2ab26eeb0
Mai Nhat Truong <truong2002hp@gmail.com>
2024-09-22 16:15:15 +0700

    Update UI

--- LV2-Gacha/Assets/Scripts/GameController.cs
+++ LV2-Gacha/Assets/Scripts/GameController.cs
@@ -5,25 +5,43 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] float maxSpeed;
+
     [SerializeField] float speed;
+
     [SerializeField] float speedDown;
+
     [SerializeField] Transform imgWheel;
+
     private bool stopWheel;
+
     [SerializeField] GameObject bgResult;
+
     [SerializeField] Text textUI;
+
     private float time;
+
     [SerializeField] List<string> prizes = new List<string>();
+
     [SerializeField] AudioSource audio;
+
     float finalAngle;
+
     float angle;
-    private float target;
+
+    private float targetAngle;
+
     [SerializeField] bool random;
+
     int indexRandom;
+
+    [SerializeField] int round;
+
     private void Start()
     {
         audio = gameObject.GetComponent<AudioSource>();
@@ -32,25 +50,32 @@ public class GameController : MonoBehaviour
     {
         if (stopWheel)
         {
-
             time -= Time.deltaTime;
-            imgWheel.transform.Rotate(0, 0, speed * time);
-            speed += speedDown;
+
+            imgWheel.transform.Rotate(0, 0, speed * time *Time.deltaTime);
+
+            speed -= speedDown;
+
             if (random)
             {
                 finalAngle = imgWheel.transform.eulerAngles.z;
-                if(finalAngle > target &&  finalAngle < target + angle && time < 1f)
+
+                if (finalAngle > targetAngle && finalAngle < targetAngle + angle && time < 1f)
                 {
+
                     stopWheel = true;
+
                     showResult(finalAngle);
+
                 }
-                
+
             }
             else
             {
                 if (time <= 0)
                 {
                     finalAngle = imgWheel.transform.eulerAngles.z;
+
                     showResult(finalAngle);
                 }
             }
@@ -61,13 +86,18 @@ public class GameController : MonoBehaviour
     private void showResult(float finalAngle)
     {
         audio.Stop();
+
         stopWheel = false;
+
         bgResult.SetActive(true);
+
         for (int i = 0; i < prizes.Count; i++)
         {
             if (finalAngle > i * angle && finalAngle < (i + 1) * angle)
             {
+
                 textUI.text = prizes[i];
+
             }
         }
     }
@@ -76,21 +106,33 @@ public class GameController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
             if (bgResult.activeSelf)
             {
+
                 bgResult.SetActive(false);
+           
             }
         }
     }
     public void Play()
     {
         angle = 360f / prizes.Count;
+
         indexRandom = Random.Range(0, prizes.Count);
+
         Debug.Log(indexRandom);
-        target = angle * indexRandom; 
+
+        targetAngle = angle * indexRandom; 
+
         time = Random.Range(4, 6);
+
+        Debug.Log("Time"+ time);
+        
         audio.Play();
+
         speed = maxSpeed;
+
         stopWheel = true;
     }
 }
--- LV2-Gacha/Assets/Scripts/GameController3D.cs
+++ LV2-Gacha/Assets/Scripts/GameController3D.cs
@@ -5,22 +5,40 @@ using UnityEngine.UI;
 
 public class GameController3D : MonoBehaviour
 {
-    [SerializeField] float maxSpeed;
+    [SerializeField] float maxSpeed; 
+
     [SerializeField] float speed;
+
     [SerializeField] float speedDown;
+
     [SerializeField] GameObject imgWheel;
+
     private bool stopWheel;
+
     [SerializeField] GameObject bgResult;
+
     [SerializeField] Text textUI;
+
     private float time;
+
     [SerializeField] List<string> prizes = new List<string>();
+
     float finalAngle;
+
     float angle;
+
     private float target;
+
     [SerializeField] bool random;
+
     int indexRandom;
+
     private static GameController3D _instantce;
+
     public static GameController3D instantce => _instantce;
+
+    [SerializeField] AudioSource audio;
+
     private void Awake()
     {
         _instantce = this;
@@ -30,15 +48,30 @@ public class GameController3D : MonoBehaviour
     {
         if (stopWheel)
         {
+
             time -= Time.deltaTime;
-            imgWheel.transform.Rotate(0, speed * time, 0);
-            speed += speedDown;
+
+            imgWheel.transform.Rotate(0, 0, speed * time * Time.deltaTime);
+
+            speed -= speedDown;
+
             if (random)
             {
-                if (finalAngle > target && finalAngle < target + angle && time < 0.5f)
+
+                if (finalAngle < 0)
                 {
+
+                    finalAngle += 360;
+
+                }
+
+                if (finalAngle > target && finalAngle < target + angle && time < 1f)
+                {
+
                     stopWheel = true;
+
                     showResult(finalAngle);
+
                 }
 
             }
@@ -46,8 +79,14 @@ public class GameController3D : MonoBehaviour
             {
                 if (time <= 0)
                 {
-                    finalAngle = imgWheel.transform.eulerAngles.y;
-                    Debug.Log(finalAngle);
+
+                    finalAngle = imgWheel.transform.eulerAngles.z;
+                    if(finalAngle < 0) {
+
+                        finalAngle += 360;
+
+                    }
+
                     showResult(finalAngle);
                 }
             }
@@ -57,13 +96,20 @@ public class GameController3D : MonoBehaviour
 
     private void showResult(float finalAngle)
     {
+        audio.Stop();
+
         stopWheel = false;
+
         bgResult.SetActive(true);
+
         for (int i = 0; i < prizes.Count; i++)
         {
+
             if (finalAngle > i * angle && finalAngle < (i + 1) * angle)
             {
+
                 textUI.text = prizes[i];
+
             }
         }
     }
@@ -72,22 +118,29 @@ public class GameController3D : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
             if (bgResult.activeSelf)
             {
+
                 bgResult.SetActive(false);
+
             }
         }
     }
     public void Play()
     {
-        Debug.Log(finalAngle);
         angle = 360f / prizes.Count;
-        indexRandom = Random.Range(0, prizes.Count);
-        Debug.Log(indexRandom);
-        target = angle * indexRandom;
-        time = Random.Range(4, 6);
-        speed = maxSpeed;
-        stopWheel = true;
 
+        indexRandom = Random.Range(0, prizes.Count);
+
+        target = angle * indexRandom;
+
+        time = Random.Range(4, 6);
+
+        audio.Play();
+
+        speed = maxSpeed;
+
+        stopWheel = true;
     }
 }
